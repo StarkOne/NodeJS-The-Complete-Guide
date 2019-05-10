e1ac0fa12b88d7f3429cda9927c145bd0f32d4a4
StarkOne <Stark.pro40@gmail.com>
2019-05-10 16:59:50 +0700

    add 404 route and admin routes path

--- app.js
+++ app.js
@@ -4,12 +4,18 @@ const bodyParser = require("body-parser");
 const app = express();
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+
 app.use(bodyParser.urlencoded({
   extended: false
 }));
 
-app.use(adminRoutes);
+app.use("/admin",adminRoutes);
 app.use(shopRoutes);
+
+// error 404 
+app.use(function (req, res, next) {
+  res.status(404).send("<h1>404 Sorry cant find that!</h1>");
+});
 
 app.listen(3000, () => {
   console.log("Сервер запущен!");
--- routes/admin.js
+++ routes/admin.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.get("/add-product", function (req, res) {
   res.send(`
-    <form action="/product" method="POST" >
+    <form action="/admin/product" method="POST" >
       <input type="text" name="title">
       <button type="submit">Add product</button>
     </form>
